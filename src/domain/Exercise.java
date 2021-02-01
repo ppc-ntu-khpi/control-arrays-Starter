43832015aa022d7566c2ac26b0ec3d073262bbca
Evgeniu603 <72415121+Evgeniu603@users.noreply.github.com>
2021-02-01 11:16:55 +0200

    Update Exercise.java

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,23 @@
 package domain;
-
+/**
+ * class with method Calculate which contain algorithm for task
+ * @author Дом
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    public static int Calculate(){
+    int[] a = {24,90,55,96};
+    int max1 = a[0];
+    int max2 = a[1];
+    for (int i = 2; i < a.length; i++){
+        if (a[i] > max2){
+            max2 = a[i];
+        }
+        if (max2 > max1){
+            int temp = max1;
+            max1 = max2;
+            max2 = temp;
+        }
+    }
+     return max2;
     }
 }
